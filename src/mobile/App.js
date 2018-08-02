665c9c1333568f1ab27c5c5c11bdfb51f250384b
GUOAB <AbbyGuo@oocl.com>
2018-08-02 17:17:43 +0800

    add login page

--- src/mobile/App.js
+++ src/mobile/App.js
@@ -1,6 +1,8 @@
 import React, {Component} from "react";
 import {Route, Switch} from "react-router-dom";
 import Index from "./component/index";
+import mobileMain from "./component/mobileMain";
+import Login from "./component/login";
 
 class App extends Component {
 
@@ -8,6 +10,8 @@ class App extends Component {
         return (
             <Switch>
                 <Route exact path='/' component={Index}/>
+                <Route exact path='/mobile/main' component={mobileMain}/>
+                <Route exact path='/mobile/login' component={Login}/>
             </Switch>
         );
     }
