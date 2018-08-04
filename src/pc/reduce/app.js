45fb7e05007f388310fc1009d6657b0284ff4359
XUHO3 <howells.xu@oocl.com>
2018-08-04 15:28:25 +0800

    update parkingBoy compent

--- src/pc/reduce/app.js
+++ src/pc/reduce/app.js
@@ -1,9 +1,10 @@
 import lot from "./lot";
 import {combineReducers} from "redux";
 import employee from "./employee";
+import parkingBoy from "./parkingBoy"
 
 const appReducer = combineReducers({
-    lot,employee
+    lot,employee,parkingBoy
 });
 
 export default appReducer
