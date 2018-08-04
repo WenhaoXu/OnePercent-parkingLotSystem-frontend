bd6662df2bed2a57c186e3d6ae6a6318c09927af
SiuKeung <siu_keung@outlook.com>
2018-08-04 15:15:02 +0800

    finished the dashboard

--- src/pc/reduce/app.js
+++ src/pc/reduce/app.js
@@ -1,9 +1,10 @@
 import lot from "./lot";
 import {combineReducers} from "redux";
 import employee from "./employee";
+import DashBoardReducer from './DashBoardReducer';
 
 const appReducer = combineReducers({
-    lot,employee
+    lot,employee, DashBoardReducer
 });
 
 export default appReducer
