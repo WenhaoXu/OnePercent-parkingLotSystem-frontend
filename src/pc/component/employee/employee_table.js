8fce05566ec4e5c2f4b0e3d7911936419fd49db0
GUOAB <AbbyGuo@oocl.com>
2018-08-04 19:07:08 +0800

    add addPop

--- src/pc/component/employee/employee_table.js
+++ src/pc/component/employee/employee_table.js
@@ -4,16 +4,6 @@ import './employee.css';
 import Employee_addPopupContainer from '../../container/employee_addPopupContainer'
 import Employee_updatePopupContainer from '../../container/employee_updatePopupContainer'
 const { Column} = Table;
-function confirm(e) {
-    console.log(e);
-    message.success('Click on Yes');
-}
-
-function cancel(e) {
-    console.log(e);
-    message.error('Click on No');
-}
-
 class Employee_table extends React.Component {
     constructor(props){
         super(props);
@@ -24,12 +14,15 @@ class Employee_table extends React.Component {
         const getChooseValuefromMap = this.props.getChooseValuefromMap;
         getChooseValuefromMap(values);
     }
-    frozen=(id)=>{
+    confirm=(id)=>{
         const forzenEmployeefromMap = this.props.forzenEmployeefromMap;
         forzenEmployeefromMap(id);
     }
+    cancel=(e)=> {
+        console.log(e);
+        message.error('Click on No');
+    }
 
-<<<<<<< HEAD
     render() {
         const data = this.props.employeeList;
         return (
@@ -65,52 +58,14 @@ class Employee_table extends React.Component {
                         key="action"
                         render={(text, record) => (
                             <span>
-=======
-  render() {
-    const data = this.props.employeeList;
-
-    return (
-      <div>
-        <Table dataSource={data} rowKey="id">
-          <Column
-              title="id"
-              dataIndex="id"
-              key="id"
-          />
-            <Column
-                title="用户名"
-                dataIndex="userName"
-                key="userName"
-            />
-          <Column
-              title="姓名"
-              dataIndex="name"
-              key="name"
-          />
-          <Column
-            title="email"
-            dataIndex="email"
-            key="email"
-          />
-          <Column
-            title="电话号码"
-            dataIndex="phone"
-            key="phone"
-          />
-          <Column
-            title="操作"
-            key="action"
-            render={(text, record) => (
-              <span>
->>>>>>> fef1d93b2b89ddccfcc55b63f76da296d02180bc
                 <a onClick={()=>this.showUpdateModal(record)}>修改</a>
                 <Divider type="vertical" />
 
-                                {/*<Popconfirm title="确定冻结此用户？" onConfirm={confirm} onCancel={cancel} okText="Yes" cancelText="No">*/}
-                                <a onClick={()=>this.frozen(record.id)}>{record.loginFlag==1?"冻结":"恢复"}</a>
-                                {/*</Popconfirm>*/}
+                {/*<Popconfirm title="确定冻结此用户？" onConfirm={this.confirm(record.id)} onCancel={this.cancel} okText="Yes" cancelText="No">*/}
+                    <a>{record.loginFlag==1?"冻结":"恢复"}</a>
+                {/*</Popconfirm>*/}
 
-                                <Divider type="vertical" />
+                <Divider type="vertical" />
               </span>
                         )}
                     />
