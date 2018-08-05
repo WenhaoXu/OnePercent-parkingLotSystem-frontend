dd0377f095e3dc5a4a6bd7a9022141c3dec173c2
LEON YU (ITA-ISDC-ISD-OOCLL/ZHA) <YULE4@OOCL.COM>
2018-08-05 19:34:17 +0800

    add Toast pupop in moblie scrambleOrder page

--- src/mobile/api/orderAPi.js
+++ src/mobile/api/orderAPi.js
@@ -1,30 +1,19 @@
 import React from 'react';
 import {scrambleOrderTurnToAccess,getOrderList1} from "../action";
 import conf from "./conf";
-import {Modal} from "antd-mobile";
+import {Toast} from "antd-mobile";
 
 function success() {
-    const modal = Modal.success({
-        title: '抢单成功',
-        content: '请尽快处理订单',
-        cancelText:''
-    });
-    setTimeout(() => modal.destroy(), 1000);
+    Toast.success('抢单成功', 1);
 }
 
 function fail() {
-    const modal = Modal.error({
-        title: '抢单失败',
-        content: '该订单已被抢走或者你的停车场已满',
-    });
-    // setTimeout(() => modal.destroy(), 1000);
+    Toast.fail('抢单失败', 1);
 }
 export const scrambleOrder1=(orderId,dispatch)=>{
 
     let item = localStorage.getItem("token");
     let parse = JSON.parse(item);
-    console.log("手机端");
-    console.log(parse.userId);
     fetch(`${conf.domain}/orders/${orderId}?operation=robOrder&coordinatorId=${parse.userId}`, {
             method: 'PATCH',
             headers:
@@ -35,7 +24,6 @@ export const scrambleOrder1=(orderId,dispatch)=>{
                     fail();
                 }else{
                     const order = response.json();
-                    console.log(order);
                     fetch(`${conf.domain}/orders/pending`, {
                         method: 'GET',
                         headers:
