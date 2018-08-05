5f2ed28ae39256d845052b436071828597803cf1
LEON YU (ITA-ISDC-ISD-OOCLL/ZHA) <YULE4@OOCL.COM>
2018-08-05 18:36:45 +0800

    add Modal pupo to hint message

--- src/mobile/api/orderAPi.js
+++ src/mobile/api/orderAPi.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import {scrambleOrderTurnToAccess,getOrderList1} from "../action";
 import conf from "./conf";
+import {Modal} from "antd-mobile";
 
+function success() {
+    const modal = Modal.success({
+        title: '抢单成功',
+        content: '请尽快处理订单',
+        cancelText:''
+    });
+    setTimeout(() => modal.destroy(), 1000);
+}
 
+function fail() {
+    const modal = Modal.error({
+        title: '抢单失败',
+        content: '该订单已被抢走或者你的停车场已满',
+    });
+    // setTimeout(() => modal.destroy(), 1000);
+}
 export const scrambleOrder1=(orderId,dispatch)=>{
 
     let item = localStorage.getItem("token");
@@ -16,27 +32,27 @@ export const scrambleOrder1=(orderId,dispatch)=>{
         })
             .then(response=> {
                 if (response.status==403){
-                    alert("抢单失败");
-                }
-                const order = response.json();
-                console.log(order);
-                fetch(`${conf.domain}/orders/pending`, {
-                    method: 'GET',
-                    headers:
-                        {'Authorization':parse.token}
-                })
-                    .then(response => response.json())
-                    .then(json => {
-                        const orderList = json;
-                        console.log("调用API获取Order列表");
-                        console.log(orderList);
-                        dispatch(scrambleOrderTurnToAccess(orderList));
-                        // history.push("/parkAndTake")
-                        // // window.location.href="/parkAndTake";
+                    fail();
+                }else{
+                    const order = response.json();
+                    console.log(order);
+                    fetch(`${conf.domain}/orders/pending`, {
+                        method: 'GET',
+                        headers:
+                            {'Authorization':parse.token}
                     })
-                    .catch(function (ex) {
-                        console.log('parsing failed', ex)
-                    });
+                        .then(response => response.json())
+                        .then(json => {
+                            const orderList = json;
+                            success();
+                            dispatch(scrambleOrderTurnToAccess(orderList));
+                            // history.push("/parkAndTake")
+                            // // window.location.href="/parkAndTake";
+                        })
+                        .catch(function (ex) {
+                            console.log('parsing failed', ex)
+                        });
+                }
             })
             .catch(function (ex) {
                 console.log('parsing failed', ex)
